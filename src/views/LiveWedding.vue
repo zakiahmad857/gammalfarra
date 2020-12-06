79b81732e730d6ef69f26ebf03b0c9ada7e3c285
Ahmad Zaki Haninut Tuqo <zakiahmad857@gmail.com>
2020-12-06 16:04:02 +0700

    Update LiveWedding.vue

--- src/views/LiveWedding.vue
+++ src/views/LiveWedding.vue
@@ -46,7 +46,7 @@
               <iframe
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/gIB2egm7tL8?rel=0&amp;autoplay=1"
+                src="https://www.youtube.com/embed/gIB2egm7tL8?autoplay=1"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
